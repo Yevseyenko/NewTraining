decbad5fc39c13c90a6ba4642f7b74231a15f1d0
Yevseyenko <jevsejen@gmail.com>
2018-11-06 08:50:02 +0200

    java 8

---
+++ src/main/java/WithJavaEight.java
@@ -0,0 +1,24 @@
+import model.User;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class WithJavaEight {
+    public static void main(String [] args){
+        List<User> users = Arrays.asList(
+          new User("John",28),
+          new User("Jane",35),
+          new User ("Alex",21));
+        System.out.println("Before sort:");
+        for(User user: users){
+            System.out.println(user);
+        }
+
+        Collections.sort(users,(o1,o2)->o1.getAge()-o2.getAge());
+        System.out.println("\nAfter sort:");
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+}
