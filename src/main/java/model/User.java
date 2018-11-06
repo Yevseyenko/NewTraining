f70bfa7a68bd6b6df79d8db4fef09c22e74d3221
Yevseyenko <jevsejen@gmail.com>
2018-11-06 08:40:16 +0200

    Initial commit

---
+++ src/main/java/model/User.java
@@ -0,0 +1,25 @@
+package model;
+
+public class User {
+    String name;
+    int age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name+" "+age;
+    }
+}
