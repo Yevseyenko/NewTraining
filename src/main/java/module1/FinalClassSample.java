26bbbe3f1d4b15012c55a624bcd1c617fbb4f928
vasyl.yevseyenko <Vasyl_Yevseyenko@epam.com>
2019-10-26 20:58:48 +0300

    add final class

--- src/main/java/module1/FinalClassSample.java
+++ src/main/java/module1/FinalClassSample.java
@@ -1,17 +1,30 @@
 package module1;
 
-public class FinalClassSample {
+public final class FinalClassSample {
     public static void main(String[] args) {
-        ChildClass childClass = new ChildClass();
+        FinalClassSample childClass = new FinalClassSample();
         childClass.printSomething("Something");
+        childClass.printNothing();
     }
 
     public void printSomething(String something) {
         System.out.println(something);
     }
+
+    public void printNothing(){
+        System.out.println("Nothing");
+    }
 }
 
-
+/*
 class ChildClass extends FinalClassSample {
-
+public void printSomethingg (String message){
+    System.out.println(message+"was printed from child");
 }
+
+@Override
+public void printNothing(){
+    System.out.println("Wowowowowowowow!");
+}
+}
+*/
